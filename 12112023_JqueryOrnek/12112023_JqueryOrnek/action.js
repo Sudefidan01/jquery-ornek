fbcfeac44983fb64f06e60db0e9b76465eba5a0b
sude <sudefidan@icloud.com>
2024-10-17 21:02:24 +0300

    jquery-ornek

---
+++ 12112023_JqueryOrnek/12112023_JqueryOrnek/action.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $('ul.tab li a ').click(function () {
+        //alert('Tab a tıklandı');
+
+        //removeClass() metodu belirtilen elementin içerisindeki class bilgisini silmek için kullanılır
+
+        $('ul.tab li a').removeClass('active');
+
+        //addClass() metodu belirtilen elementin içerisine belirtilen class bilgisini eklemek için kullanılır
+
+        $(this).addClass('active');
+
+        var dataShow = $(this).data('show');
+
+        $('div.tabIcerik > div').slideUp();
+        $(dataShow).slideDown();
+
+    });
+
+
+});
